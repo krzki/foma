d44db4f94f9419917aa8fa85f8e0701833372253
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2023-02-28 17:58:42 +0700

    Fix test

--- foma/python/test/test_foma.py
+++ foma/python/test/test_foma.py
@@ -3,27 +3,33 @@
 
 """
 Test cases for Foma Python bindings.
+
+english.bin is taken from https://fomafst.github.io/morphtut.html
 """
 
 import pytest
-from foma import FST
-
+from src.foma_with_lib.foma import FST
+import os
 
 def test_load_fst():
-    fst = FST.load('ate.fsm')
+    dir_path, _ = os.path.split(os.path.realpath(__file__))
+    foma_script_path = os.path.join(dir_path, 'english.bin')
+    fst = FST.load(foma_script_path)
     assert isinstance(fst, FST)
 
 
 def test_apply_fst(eat_fst):
-    result, = eat_fst.apply_up('ate')
-    assert result == 'eat+V+Past'
+    result, = eat_fst.apply_up('cat')
+    assert result == 'cat+N+Sg'
 
 
 def test_apply_down(eat_fst):
-    result, = eat_fst.apply_down('eat+V+3P+Sg')
-    assert result == 'eats'
+    result, = eat_fst.apply_down('fox+N+Sg')
+    assert result == 'fox'
 
 
 @pytest.fixture
 def eat_fst():
-    return FST.load('ate.fsm')
+    dir_path, _ = os.path.split(os.path.realpath(__file__))
+    foma_script_path = os.path.join(dir_path, 'english.bin')
+    return FST.load(foma_script_path)
